28f40f9c516838b6ff99f4cd9b0a5afc38dcdd22
Douvry <valentin.jabba@gmail.com>
2020-08-18 23:56:05 -0400

    Base site

---
+++ js/carousel.js
@@ -0,0 +1,18 @@
+$('.carousel.carousel-slider').carousel({
+    fullWidth: true,
+    indicators: true
+});
+
+// move next carousel
+$('.moveNextCarousel').click(function (e) {
+    e.preventDefault();
+    e.stopPropagation();
+    $('.carousel').carousel('next');
+});
+
+// move prev carousel
+$('.movePrevCarousel').click(function (e) {
+    e.preventDefault();
+    e.stopPropagation();
+    $('.carousel').carousel('prev');
+});
